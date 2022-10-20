74860e635767389865c7e60e720db4c998918ec6
Nathaniel Tornow <nathaniel.tornow@tum.de>
2022-10-20 12:37:53 +0200

    adding qernel from qiskit

--- qstack/qernel/qernel.py
+++ qstack/qernel/qernel.py
@@ -1,39 +1,80 @@
 from dataclasses import dataclass
-from typing import Dict, Union
+from typing import Dict, Optional, Sequence, Union
 
-from qiskit.circuit import QuantumCircuit, Gate, Parameter
-
-
-class UnaryPlaceholder(Gate):
-    def __init__(self, name: str) -> None:
-        super().__init__(name, 1, [])
-
-
-class BinaryPlaceholder(Gate):
-    def __init__(self, name: str) -> None:
-        super().__init__(name, 2, [])
+from qiskit.circuit.quantumcircuit import (
+    QuantumCircuit,
+    Qubit,
+    Bit,
+    Register,
+    InstructionSet,
+    ParameterValueType,
+    Instruction,
+    Parameter,
+)
 
 
 @dataclass
-class Input:
+class QernelArgs:
+    """Input to a Qernel"""
+
     params: Dict[Parameter, float]
-    subcircuits: Dict[Union[UnaryPlaceholder, BinaryPlaceholder], QuantumCircuit]
+    subcircs: Dict[str, QuantumCircuit]
+
+
+class UnaryPlaceholder(Instruction):
+    """A unary placeholder to insert to a qernel"""
+
+    def __init__(self, name: str, clbit: bool = False):
+        num_clbits = 1 if clbit else 0
+        super().__init__(name=name, num_qubits=1, num_clbits=num_clbits, params=[])
+
+    def _define(self):
+        self._definition = QuantumCircuit(1, name=self.name)
 
 
 class Qernel(QuantumCircuit):
-    @staticmethod
-    def from_circuit(circuit: QuantumCircuit) -> "Qernel":
-        qernel = Qernel(
-            *circuit.qregs,
-            *circuit.cregs,
-            name=circuit.name,
-            global_phase=circuit.global_phase,
-            metadata=circuit.metadata
-        )
-        for instr in circuit.data:
-            qernel.append(instr)
-        return qernel
+    # placeholder name to instruction indices
 
-    def with_inserted_inputs(self, input: Input) -> "Qernel":
-        # TODO
-        pass
+    def __init__(
+        self,
+        *regs: Union[Register, int, Sequence[Bit]],
+        name: Optional[str] = None,
+        global_phase: ParameterValueType = 0,
+        metadata: Optional[Dict] = None,
+    ):
+        super().__init__(*regs, name=name, global_phase=global_phase, metadata=metadata)
+
+    def placeholder(
+        self,
+        name: str,
+        qubit: Union[int, Qubit],
+        clbit: Optional[Union[int, Qubit]] = None,
+    ) -> InstructionSet:
+
+        if clbit is not None:
+            return self.append(
+                UnaryPlaceholder(name=name, clbit=True), [qubit], [clbit]
+            )
+        else:
+            return self.append(UnaryPlaceholder(name=name), [qubit], [])
+
+    def with_input(self, args: QernelArgs) -> QuantumCircuit:
+        res_circ = QuantumCircuit(*self.qregs, *self.cregs, name=self.name)
+        for instr in self.data:
+            if isinstance(instr, UnaryPlaceholder):
+                if instr.name not in args.subcircs:
+                    raise ValueError(
+                        f"Placeholder {instr.name} is not present in input"
+                    )
+                subcirc = args.subcircs[instr.name]
+                if subcirc.num_qubits != 1 or subcirc.num_clbits > 1:
+                    raise ValueError(
+                        "Subcircuit must have 1 qubit and 0 or 1 clbits, not {} qubits and {} clbits".format(
+                            subcirc.num_qubits, subcirc.num_clbits
+                        )
+                    )
+                res_circ.append(subcirc.to_instruction(), instr.qubits, instr.clbits)
+            else:
+                res_circ.append(instr)
+
+        return res_circ.bind_parameters(args.params)
