d5e6b34d4f42616d86ee22635e33ac554179a1f5
Emmanouil (Manos) Giortamis <giortan13@gmail.com>
2022-10-26 14:47:50 +0200

    Create merge_engine.py
    API for merge engine

---
+++ qstack/qos/merge_engine.py
@@ -0,0 +1,20 @@
+from qiskit.circuit.quantumcircuit import QuantumCircuit
+from qstack.qernel.qernel import Qernel, QernelArgs
+from qstack.types import QPUWrapper
+
+
+class MergeEngine:
+    """QOS engine for merging qernels based on a matching score"""
+    
+    default_qpu = None
+    
+    def __init__(self, qpu : QPUWrapper = None) -> None:
+        self.default_qpu = qpu
+        
+    def merge_qernels(self, qernel1: Qernel, qernel2: Qernel, qpu: QPUWrapper = None, forced: bool = False) -> Qernel:
+    """ Merge Qernels if compatible or if forced=True"""
+        pass
+    
+    def get_matching_score(self, qernel1: Qernel, qernel2: Qernel, qpu: QPUWrapper = None) -> float:
+    """ Get a matching score in [0,1] for two Qernels"""
+        pass
