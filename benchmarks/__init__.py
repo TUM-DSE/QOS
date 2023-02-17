086fb93128ef83810a4dbc8b7383d3b4b51f2fec
store <franciscoromao@tecnico.ulisboa.pt>
2023-02-17 16:54:51 +0100

    FermionicQAOA implemented

--- benchmarks/__init__.py
+++ benchmarks/__init__.py
@@ -1,6 +1,6 @@
 from .chemistry import HamiltonianSimulationBenchmark, VQEBenchmark
 from .error_correction import BitCodeBenchmark, PhaseCodeBenchmark
-from .optimization import VanillaQAOABenchmark
+from .optimization import VanillaQAOABenchmark, FermionicSwapQAOABenchmark
 from .quantum_information import GHZBenchmark
 from ._types import Device
 from ._utils import perfect_counts, fidelity
