fa4fe2aa4017cf480834defdd799d6b36d501bfd
FranciscoRomao <franciscoromao@tecnico.ulisboa.pt>
2023-03-23 17:26:46 +0100

    Working on increasing fragments experiment

--- executor.py
+++ executor.py
@@ -6,105 +6,123 @@ from qiskit.providers.fake_provider import *
 import matplotlib.pyplot as plt
 from benchmarks import *
 from collections import Counter
-#from ._utils import perfect_counts, fidelity
+from qiskit_aer.noise import NoiseModel
+
+# from ._utils import perfect_counts, fidelity
+
 
 def move_and_rename_file(src_path, dst_dir, new_name):
     dst_path = os.path.join(dst_dir, new_name)
-    #print(dst_path)
+    # print(dst_path)
     # Move the file to the destination path
     shutil.copy2(src_path, dst_path)
 
 
-
-def iterate_files_in_directory(dir_path):    
+def iterate_files_in_directory(dir_path):
     bench = VanillaQAOABenchmark(7)
     qc = bench.circuit()
 
     for filename in os.listdir(dir_path):
         # Construct the full path to the file by joining the directory path and the filename
         file_path = os.path.join(dir_path, filename)
-        #print(file_path)
-        
+        # print(file_path)
+
         # Check if the path is a file (as opposed to a directory or a symlink)
         if os.path.isfile(file_path):
-            move_and_rename_file(file_path, "/mnt/c/Users/giort/Documents/GitHub/ME/lib/python3.9/site-packages/qiskit/test/mock/backends/lagos", "props_lagos.json")
+            move_and_rename_file(
+                file_path,
+                "/mnt/c/Users/giort/Documents/GitHub/ME/lib/python3.9/site-packages/qiskit/test/mock/backends/lagos",
+                "props_lagos.json",
+            )
 
             backend = FakeLagosV2()
             fids = []
-            
-            for i in range (5):
+
+            for i in range(5):
                 qc = transpile(qc, backend)
-                
+
                 result = backend.run(qc, shots=8192).result()
                 counts = result.get_counts()
-                #avg_fid = avg_fid + hellinger_fidelity(perf_counts, counts)
+                # avg_fid = avg_fid + hellinger_fidelity(perf_counts, counts)
                 fids.append(bench.score(Counter(counts)))
-                
+
             fids.sort()
             print(fids[2])
-            
 
-#iterate_files_in_directory("/mnt/c/Users/giort/Documents/GitHub/qos/callibration_data")
+
+# iterate_files_in_directory("/mnt/c/Users/giort/Documents/GitHub/qos/callibration_data")
+
 
 def execute_on_backends():
     backends = [
-    "Casablanca",
-    "Jakarta",
-    "Lagos",
-    "Almaden",
-    "Melbourne",
-    "Guadalupe",
-    "Boeblingen",
-    "Singapore",
-    "Johannesburg",
-    "Cairo",
-     "Hanoi",
-     "Paris",
-     "Sydney",
-     "Toronto",
-     "Kolkata",
-     "Montreal",
-     "Cambridge",
-     "Washington"]
-     
+        "Casablanca",
+        "Jakarta",
+        "Lagos",
+        "Almaden",
+        "Melbourne",
+        "Guadalupe",
+        "Boeblingen",
+        "Singapore",
+        "Johannesburg",
+        "Cairo",
+        "Hanoi",
+        "Paris",
+        "Sydney",
+        "Toronto",
+        "Kolkata",
+        "Montreal",
+        "Cambridge",
+        "Washington",
+    ]
+
     bench = HamiltonianSimulationBenchmark(7)
     qc = bench.circuit()
-    
+
     fids = {}
-    
+
     for b in backends:
         fids[b] = []
         backend = eval("Fake" + b + "V2()")
         cqc = transpile(qc, backend)
-        
-        for i in range(5):  
+
+        for i in range(5):
             result = backend.run(cqc, shots=8192).result()
             counts = result.get_counts()
-            #avg_fid = avg_fid + hellinger_fidelity(perf_counts, counts)
+            # avg_fid = avg_fid + hellinger_fidelity(perf_counts, counts)
             fids[b].append(bench.score(Counter(counts)))
-                     
-        #print(fids)
-        #fids.sort()
+
+        # print(fids)
+        # fids.sort()
         print(fids[b])
-        
+
     means = [np.median(fids[name]) for name in fids]
     stds = [np.std(fids[name]) for name in fids]
     diff_all = np.max(np.abs(np.subtract.outer(means, means)))
-    
 
     fig, ax = plt.subplots()
-    ax.bar(fids.keys(), means, yerr=stds, align='center', alpha=0.5, ecolor='black', capsize=10)
-    ax.axhline(np.mean(means), color='green', linewidth=2)
-    plt.text(0.01, 0.95, "Max difference: {:.2f}".format(diff_all),
-             transform=plt.gca().transAxes)
-    ax.set_xlabel('IBMQ Backend')
-    ax.set_ylabel('Fidelity Score')
-    plt.xticks(rotation='vertical')
+    ax.bar(
+        fids.keys(),
+        means,
+        yerr=stds,
+        align="center",
+        alpha=0.5,
+        ecolor="black",
+        capsize=10,
+    )
+    ax.axhline(np.mean(means), color="green", linewidth=2)
+    plt.text(
+        0.01,
+        0.95,
+        "Max difference: {:.2f}".format(diff_all),
+        transform=plt.gca().transAxes,
+    )
+    ax.set_xlabel("IBMQ Backend")
+    ax.set_ylabel("Fidelity Score")
+    plt.xticks(rotation="vertical")
     ax = plt.gca()
     ax.set_ylim([0.5, 1])
-    #ax.set_title('GHZ fidelity score across IBMQ Backends')
-    plt.savefig('plot.png', dpi=300, bbox_inches="tight")
-            
+    # ax.set_title('GHZ fidelity score across IBMQ Backends')
+    plt.savefig("plot.png", dpi=300, bbox_inches="tight")
 
 
 def run_all_benchs(dir_path):
@@ -117,19 +135,16 @@ def run_all_benchs(dir_path):
         perf_counts = perfect_counts(qc)
         qc = transpile(qc, backend, optimization_level=3)
         fids = []
-        
+
         for i in range(5):
             job = backend.run(qc, shots=8192)
 
             counts = job.result().get_counts()
-            
+
             fids.append(fidelity(perf_counts, counts))
         print(fids)
         fids.sort()
         print(fids[2])
-        
-
-
 
 
 def execute_benchmarks():
@@ -141,80 +156,89 @@ def execute_benchmarks():
         "BitCode",
         "PhaseCode",
         "MerminBell",
-        "FermionicSwapQAOA"
+        "FermionicSwapQAOA",
     ]
-    
-    backend = FakeGuadalupeV2()
-    
+
+    backend = FakeWashingtonV2()
+    backend_noise = NoiseModel.from_backend(backend)
+
     qbits = [3, 5, 7, 9]
-    #qbits = [2, 3]
-    
+    # qbits = [2, 3]
+
     fids = {}
-    
+
     for q in qbits:
         key = str(q) + "q"
         fids[key] = {}
-        
+
         for b in benchmarks:
             fids[key][b] = []
             print(q, b)
             if b == "PhaseCode" or b == "BitCode":
-                bench = eval(b + "Benchmark")(int(q/2) + 1)
+                bench = eval(b + "Benchmark")(int(q / 2) + 1)
             elif b == "VQE":
-                bench = eval(b + "Benchmark")(int(q/2))
+                bench = eval(b + "Benchmark")(int(q / 2))
             else:
                 bench = eval(b + "Benchmark")(q)
-            
+
             qc = bench.circuit()
             cqc = transpile(qc, backend)
-            
-            for i in range(5):  
+
+            for i in range(5):
                 result = backend.run(cqc, shots=8192).result()
                 counts = result.get_counts()
                 counts_copy = {}
-                               
-                
+
                 if isinstance(counts, list):
                     fids[key][b].append(bench.score(counts))
                 else:
                     for (k, v) in counts.items():
                         counts_copy[k.replace(" ", "")] = v
-                    
+
                     fids[key][b].append(bench.score(Counter(counts_copy)))
 
-                     
-    #means = { q : [np.median(fids[b]) for b in fids[qbits]] for qbits in fids}
+    # means = { q : [np.median(fids[b]) for b in fids[qbits]] for qbits in fids}
     means = {}
     for q, b in fids.items():
         means[q] = [np.median(fids[q][b]) for b in fids[q]]
-    
+
     stds = {}
     for q, b in fids.items():
         stds[q] = [np.std(fids[q][b]) for b in fids[q]]
-    #diff_all = np.max(np.abs(np.subtract.outer(means, means)))
-    
-    group_labels = ["Hamiltonian", "VQE", "VanillaQAOA", "GHZ", "BitCode", "PhaseCode", "MerminBell", "FermionicQAOA"]
-    
+    # diff_all = np.max(np.abs(np.subtract.outer(means, means)))
+
+    group_labels = [
+        "Hamiltonian",
+        "VQE",
+        "VanillaQAOA",
+        "GHZ",
+        "BitCode",
+        "PhaseCode",
+        "MerminBell",
+        "FermionicQAOA",
+    ]
+
     x = np.arange(len(group_labels))  # the label locations
     width = 0.13  # the width of the bars
     multiplier = 0
 
-    #fig, ax = plt.subplots(layout='constrained')
+    # fig, ax = plt.subplots(layout='constrained')
     fig, ax = plt.subplots()
 
     for qbits, score in means.items():
         offset = width * multiplier
         rects = ax.bar(x + offset, score, width, label=qbits, yerr=stds[qbits])
-        #ax.bar_label(rects, padding=3)
+        # ax.bar_label(rects, padding=3)
         multiplier += 1
 
     ax.set_xlabel("Benchmarks")
     ax.set_ylabel("Fidelity Score")
     ax.set_xticks(x + 2 * width, group_labels, rotation=90)
     # Add title and legend
-    #ax.set_title("Benchmark score with increasing number of qubits")
+    # ax.set_title("Benchmark score with increasing number of qubits")
     ax.legend()
-        
-    plt.savefig('plot.png', dpi=300, bbox_inches="tight")
-    
+
+    plt.savefig("plot.png", dpi=300, bbox_inches="tight")
+
+
 execute_benchmarks()
--- main.py
+++ main.py
@@ -10,11 +10,13 @@ from qiskit.compiler import transpile
 from qiskit.visualization import plot_histogram, plot_circuit_layout, plot_coupling_map
 from collections import Counter
 from qiskit.transpiler import Layout
+from qiskit_aer.noise import NoiseModel
 import csv
 import os.path
-import time 
+import time
 
 from qiskit.circuit import QuantumCircuit
+
 
 class dict2obj(object):
     def __init__(self, d):
@@ -23,6 +25,7 @@ class dict2obj(object):
                 setattr(self, k, [dict2obj(x) if isinstance(x, dict) else x for x in v])
             else:
                 setattr(self, k, dict2obj(v) if isinstance(v, dict) else v)
+
 
 def layout_to_qubit_coordinates(layout):
     """
@@ -33,7 +36,6 @@ def layout_to_qubit_coordinates(layout):
         list: A list of qubit coordinates.
     """
     layout.input_qubit_mapping.keys()
-    
 
 
 def merge_circs(q1: QuantumCircuit, q2: QuantumCircuit) -> QuantumCircuit:
@@ -74,15 +76,17 @@ def split_counts_bylist(counts, kl):
 
     return counts_list
 
+
 def getCNOTS(c: QuantumCircuit) -> int:
     ops = c.count_ops()
     cnots = 0
-    
+
     for (key, value) in ops.items():
-            if key == 'cx':
-                cnots = value
-                break
+        if key == "cx":
+            cnots = value
+            break
     return cnots
+
 
 class App:
     benchmarks = []
@@ -100,14 +104,14 @@ class App:
     rounds = 0
     bench_args = ""
     config_file = sys.argv[1]
-    static = False 
+    static = False
 
     # def __init__(self, backend, benchmark, nbits, nruns, filepath='', shots=1024):
     def __init__(self, *kwargs):
 
         config = self.config_parser(self.config_file)
         print("Working on", self.config_file)
-        #print all the config file parameters
+        # print all the config file parameters
 
         # pdb.set_trace()
         # pprint.pprint(data)
@@ -145,15 +149,22 @@ class App:
         # If one of the benchmarks requires to indicate the number of rounds then you need to put the same number of round's args as
         # the number of benchmarks, if the benchmark does not take rounds just put 0, for example:
         # `python main.py -backend FakeTorontoV2 -benchmarks GHZBenchmark BitCodeBenchmark -bits 2 3 -rounds 0 2 -runs 3 -shots 4000
-        
+
         self.bench_args = []
 
         for i in range(len(config.benchmarks)):
-            bench_args = [self.nqbits[i], self.rounds[i], self.nlayers[i], self.time_step[i], self.total_time[i], self.initial_state[i]]
-            #print(bench_args)
-            self.bench_args.append(list(filter(lambda x:x!=None, bench_args)))
-            
-        #print(self.bench_args)
+            bench_args = [
+                self.nqbits[i],
+                self.rounds[i],
+                self.nlayers[i],
+                self.time_step[i],
+                self.total_time[i],
+                self.initial_state[i],
+            ]
+            # print(bench_args)
+            self.bench_args.append(list(filter(lambda x: x != None, bench_args)))
+
+        # print(self.bench_args)
         # print(self.bench_args)
 
         # This is a very ugly inline if else statements but it works, basically what it does is:
@@ -167,8 +178,7 @@ class App:
         #   [self.nqbits] if self.rounds == None else [self.nqbits, self.rounds]
         # )
 
-
-        #If you get an error here either you are inputting more or less arguments that the benchmark needs or the initial_state does not have the
+        # If you get an error here either you are inputting more or less arguments that the benchmark needs or the initial_state does not have the
         # same number of initial values as the number of inputted qbits (Error correction)
         for idx, b in enumerate(config.benchmarks):
             self.benchmarks.append(eval(b.name)(*self.bench_args[idx]))
@@ -177,22 +187,26 @@ class App:
 
         for b in self.benchmarks:
             self.circuits.append(b.circuit())
-        
+
         for i, c in enumerate(self.circuits):
             if isinstance(c, list):
                 self.nqbits[i] = [c[0].num_qubits] * 2
             else:
                 self.nqbits[i] = c.num_qubits
 
-        #print(self.benchmarks)
-        #print(self.circuits[0])
-        #print(self.circuits[0][0].draw())
-        #pdb.set_trace()
+        # print(self.benchmarks)
+        # print(self.circuits[0])
+        # print(self.circuits[0][0].draw())
+        # pdb.set_trace()
 
         for i, b in enumerate(self.bench_args):
-            if self.rounds[i] != None: # This means that the benchmark is of the type ErrorCorrection.
+            if (
+                self.rounds[i] != None
+            ):  # This means that the benchmark is of the type ErrorCorrection.
                 self.total_bits.append(self.nqbits[i] + (b[0] - 1) * b[1])
-            elif isinstance(self.nqbits[i], list): #This means that this benchmark has more than one circuit which only happens on VQE
+            elif isinstance(
+                self.nqbits[i], list
+            ):  # This means that this benchmark has more than one circuit which only happens on VQE
                 self.total_bits.append(self.nqbits[i][0] * 2)
             else:
                 self.total_bits.append(self.nqbits[i])
@@ -200,7 +214,7 @@ class App:
         # self.benchmark = eval(args.benchmark)(*self.bench_args)
         # args.benchmark is a list of the benchmarks inputted as "-benchmark GHZBenchmark HamiltonianSimulationBenchmark" for example
 
-        # self.provider = IBMQ.load_account()
+        self.provider = IBMQ.load_account()
         self.backend = IBMQPU(self.backend, self.provider)
 
         # print(self.nqbits)
@@ -224,9 +238,8 @@ class App:
 
         # Print the data dictionary
         print(data)
-        print('\n')
+        print("\n")
 
-        
         data = dict2obj(data)
 
         for i, j in enumerate(data.config.benchmarks):
@@ -252,7 +265,7 @@ class App:
         # )
         # print(prf_counts)
 
-        #pdb.set_trace()
+        # pdb.set_trace()
 
         for i, a in enumerate(self.circuits):
             if isinstance(a, list):
@@ -260,30 +273,30 @@ class App:
 
         qc = self.circuits[0]
 
-        #print(qc)
+        # print(qc)
         # print(qc.num_qubits)
         # print(qc.num_clbits)
         # print(qc)
-                   
+
         ncircs = len(self.circuits)
-    
+
         for q in self.circuits:
             print(q.depth())
-        
+
         if ncircs > 1:
             qc = merge_circs(self.circuits[0], self.circuits[1])
 
             for i in range(2, ncircs):
                 qc = merge_circs(qc, self.circuits[i])
 
-        #prf_cnts = perfect_counts(qc)
-        #print(qc)
+        # prf_cnts = perfect_counts(qc)
+        # print(qc)
         depth_b4 = 0
         cnot_b4 = 0
-        
+
         depth_b4 = qc.depth()
         cnot_b4 = getCNOTS(qc)
-        
+
         backend = self.backend.backend
         # print(backend.name)
         nqbits = self.backend.backend.num_qubits
@@ -299,70 +312,70 @@ class App:
         utilization = utilization / nqbits
 
         # print(utilization)
- 
+
         median_fids = [] * self.nbenchmarks
 
         for i in range(self.nbenchmarks):
             median_fids.append([0] * self.nruns)
- 
+
         for k in range(self.nruns):
             try:
-                #qc = transpile(qc, backend, optimization_level=3, scheduling_method='as_late_as_possible')
-               qc = transpile(qc, backend, optimization_level=3)
+                # qc = transpile(qc, backend, optimization_level=3, scheduling_method='as_late_as_possible')
+                qc = transpile(qc, backend, optimization_level=3)
             except e:
                 print("Probably the circuit is too large for this backend. Skipping...")
                 traceback.print_exc()
                 exit(0)
-            #print(qc)
-            
+            # print(qc)
+
             depth_after = 0
             cnot_after = 0
-            
+
             depth_after = qc.depth()
             cnot_after = getCNOTS(qc)
 
-            #print(depth_b4, "\t", depth_after)
-            #print(cnot_b4, "\t", cnot_after)
-            
+            # print(depth_b4, "\t", depth_after)
+            # print(cnot_b4, "\t", cnot_after)
+
             if self.static:
                 csv_headers = []
-        
+
                 for i in range(self.nbenchmarks):
                     csv_headers.append("bench" + str(i) + "_name")
                     csv_headers.append("bench" + "_qbits")
-                #append to headers the depth and cnot before and after transpilation
+                # append to headers the depth and cnot before and after transpilation
                 csv_headers.append("depth_before")
                 csv_headers.append("depth_after")
                 csv_headers.append("cnot_before")
                 csv_headers.append("cnot_after")
-                csv_headers.append('utilization')
-                csv_headers.append('backend')
-                csv_headers.append('config_file')
+                csv_headers.append("utilization")
+                csv_headers.append("backend")
+                csv_headers.append("config_file")
 
-                #write rows with the information on the headers
-                file_exists = os.path.isfile('results/results.csv')
+                # write rows with the information on the headers
+                file_exists = os.path.isfile("results/results.csv")
 
                 with open("results/results.csv", mode="a", newline="") as csvfile:
 
                     writer = csv.writer(csvfile)
-                    
+
                     if not file_exists:
                         writer.writerow(csv_headers)
-                    
+
                     row = []
-                    
+
                     for i in range(self.nbenchmarks):
-                        #write benchmark name and qbits
+                        # write benchmark name and qbits
                         row.append(self.benchmarks[i].name())
-                        
-                        #check if nqbits is a list, if yes just append the first element if not append the whole list
+
+                        # check if nqbits is a list, if yes just append the first element if not append the whole list
                         if isinstance(self.nqbits[i], list):
                             row.append(self.nqbits[i][0])
                         else:
                             row.append(self.nqbits[i])
 
-                    #write depths and cnts before and after transpilation
-                    #merge the following rows into a single one
+                    # write depths and cnts before and after transpilation
+                    # merge the following rows into a single one
                     row.append(depth_b4)
                     row.append(depth_after)
                     row.append(cnot_b4)
@@ -370,27 +383,32 @@ class App:
                     row.append(utilization)
                     row.append(self.backend.backend.name)
                     row.append(self.config_file)
-                    
+
                     writer.writerow(row)
 
-                    #pdb.set_trace()
-                    this = layout_to_qubit_coordinates(qc._layout)
+                    # pdb.set_trace()
+                    # this = layout_to_qubit_coordinates(qc._layout)
 
-
-                    plot_coupling_map(num_qubits=self.backend.backend.num_qubits, coupling_map=this, filename="results/coupling_map" + ".png", figsize=(10, 10))
-
-
+                    # plot_coupling_map(
+                    #    num_qubits=self.backend.backend.num_qubits,
+                    #    coupling_map=this,
+                    #    filename="results/coupling_map" + ".png",
+                    #    figsize=(10, 10),
+                    # )
 
                     csvfile.close()
                 exit()
 
             if self.backend.is_simulator:
-                job = backend.run(run_input=qc, shots=self.nshots)
+                backend_noise = NoiseModel.from_backend(backend)
+                simulator = self.provider.get_backend("ibmq_qasm_simulator")
+                job = simulator.run(qc, shots=self.nshots, noise_model=backend_noise)
+                # job = backend.run(run_input=qc, shots=self.nshots)
             else:
                 job = backend.run(circuits=qc, shots=self.nshots)
             counts = job.result().get_counts()
             # splitted_counts = split_counts(counts, self.nbenchmarks)
-            #print(counts)
+            # print(counts)
 
             # plot_histogram(
             # counts, filename="results/counts" + ".png", figsize=(10, 10)
@@ -411,7 +429,7 @@ class App:
             # figsize=(10, 10),
             # )
             # print(splitted_counts)
-
+            """
             for i in range(self.nbenchmarks):
 
                 if isinstance(splitted_counts[i], list):
@@ -432,6 +450,7 @@ class App:
                         filename=self.filepath + self.filename + str(i),
                         figsize=(10, 10),
                     )
+            """
             # self.backend.backend.coupling_map.draw()
             # print(len(self.backend.backend.coupling_map))
             # plot_circuit_layout(qc, self.backend.backend)
@@ -444,43 +463,44 @@ class App:
                     median_fids[i][k] += self.benchmarks[i].score(splitted_counts[i])
                     # avg_fids[i] += tmp_fid / 2
                 else:
-                    #print(fidelity(prf_cnts, splitted_counts[i]))
-                    #median_fids[i][k] += self.benchmarks[i].score(Counter(splitted_counts[i]))
-                    median_fids[i][k] += self.benchmarks[i].score(Counter(splitted_counts[i]))
-                    #print(splitted_counts[i])
+                    # print(fidelity(prf_cnts, splitted_counts[i]))
+                    # median_fids[i][k] += self.benchmarks[i].score(Counter(splitted_counts[i]))
+                    median_fids[i][k] += self.benchmarks[i].score(
+                        Counter(splitted_counts[i])
+                    )
+                    # print(splitted_counts[i])
 
             # f.write(str(counts) + "\n")
 
-        
         fids = []
         print(median_fids)
-        
+
         for i in range(self.nbenchmarks):
             median_fids[i].sort()
-        
+
         for i in range(self.nbenchmarks):
             fids.append(median_fids[i][int(self.nruns / 2)])
-            
+
         avg_fid = 0
-           
+
         csv_headers = []
-        
+
         for i in range(self.nbenchmarks):
             csv_headers.append("bench" + str(i + 1))
             csv_headers.append("bench" + str(i + 1) + "_qbits")
-            csv_headers.append("bench" + str(i + 1) + "_fid")            
-            
-        #for i in range(self.nbenchmarks):
-            #csv_headers.append("bench" + str(i + 1) + "_fid")
-        
-        csv_headers.append('median_fid')
-        csv_headers.append('utilization')
-        csv_headers.append('backend')
-        csv_headers.append('config_file')
+            csv_headers.append("bench" + str(i + 1) + "_fid")
 
-        #data_example = ['Bench1Name', 'Bench2Name', 'Bench1FID', 'Bench2FID', 90.5, 0.75, 'Config1']
+        # for i in range(self.nbenchmarks):
+        # csv_headers.append("bench" + str(i + 1) + "_fid")
 
-        file_exists = os.path.isfile('results/results.csv')
+        csv_headers.append("median_fid")
+        csv_headers.append("utilization")
+        csv_headers.append("backend")
+        csv_headers.append("config_file")
+
+        # data_example = ['Bench1Name', 'Bench2Name', 'Bench1FID', 'Bench2FID', 90.5, 0.75, 'Config1']
+
+        file_exists = os.path.isfile("results/results.csv")
 
         with open("results/results.csv", mode="a", newline="") as csvfile:
             writer = csv.writer(csvfile)
@@ -489,41 +509,41 @@ class App:
                 writer.writerow(csv_headers)
 
             data = []
-            
+
             for i in range(self.nbenchmarks):
                 data.append(self.benchmarks[i].name())
                 data.append(self.nqbits[i])
                 data.append(fids[i])
-                
-            
-            #for i in range(self.nbenchmarks):
-                #data.append(avg_fids[i])
 
-            data.append(str(round(sum(fids)/self.nbenchmarks,2)))
-            data.append(str(round(utilization,3)))
-            
+            # for i in range(self.nbenchmarks):
+            # data.append(avg_fids[i])
+
+            data.append(str(round(sum(fids) / self.nbenchmarks, 2)))
+            data.append(str(round(utilization, 3)))
+
             data.append(self.backend.backend.name)
             data.append(self.config_file)
 
             writer.writerow(data)
             # This is ugly to have to lines with the same information just with the benchmarks swapped, but it is easier to process
             data = []
-            
+
             for i in range(self.nbenchmarks - 1, -1, -1):
                 data.append(self.benchmarks[i].name())
                 data.append(self.nqbits[i])
                 data.append(fids[i])
-            
-            #for i in range(self.nbenchmarks):
-                #data.append(avg_fids[-(i+1)])
-                
-            data.append(str(round(sum(fids)/self.nbenchmarks,2)))
-            data.append(str(round(utilization,3)))
-            
+
+            # for i in range(self.nbenchmarks):
+            # data.append(avg_fids[-(i+1)])
+
+            data.append(str(round(sum(fids) / self.nbenchmarks, 2)))
+            data.append(str(round(utilization, 3)))
+
             data.append(self.backend.backend.name)
             data.append(self.config_file)
 
             writer.writerow(data)
+
 
 app = App(sys.argv)
 app.run()
