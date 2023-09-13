2373ec74f750e4e13217787e2411469b860a4ecf
manosgior <giortan13@gmail.com>
2023-09-13 19:09:46 +0200

    Update passes_test.py

--- passes_test.py
+++ passes_test.py
@@ -67,17 +67,17 @@ def main():
     #qc = qc.decompose()
     #print(qc)
 
-    qc_full = QuantumCircuit.from_qasm_file("~/Downloads/FrozenQubits_data_and_sourcecode/experiments/qaoa/sk/gridsearch_100/ideal/3_4_1^P=1.qasm")
-    qc_frozen1 = QuantumCircuit.from_qasm_file("~/Downloads/FrozenQubits_data_and_sourcecode/experiments/frozenqubits_full/sk/gridsearch_100/ideal/3_4_1^M=1_0^P=1.qasm")
-    qc_frozen2 = QuantumCircuit.from_qasm_file("~/Downloads/FrozenQubits_data_and_sourcecode/experiments/frozenqubits_full/sk/gridsearch_100/ideal/3_4_1^M=1_1^P=1.qasm")
-    print(qc_full)
-    print(qc_frozen1)
-    print(qc_frozen2)   
+    #qc_full = QuantumCircuit.from_qasm_file("~/Downloads/FrozenQubits_data_and_sourcecode/experiments/qaoa/sk/gridsearch_100/ideal/3_4_1^P=1.qasm")
+    #qc_frozen1 = QuantumCircuit.from_qasm_file("~/Downloads/FrozenQubits_data_and_sourcecode/experiments/frozenqubits_full/sk/gridsearch_100/ideal/3_4_1^M=1_0^P=1.qasm")
+    #qc_frozen2 = QuantumCircuit.from_qasm_file("~/Downloads/FrozenQubits_data_and_sourcecode/experiments/frozenqubits_full/sk/gridsearch_100/ideal/3_4_1^M=1_1^P=1.qasm")
+    #print(qc_full)
+    #print(qc_frozen1)
+    #print(qc_frozen2)   
 
     #circuits = [qc_full, qc_frozen1, qc_frozen2]
 
-    #provider =  IBMProvider(instance="ibm-q-research-2/tu-munich-1/main")
-    #backend = provider.get_backend("ibm_nairobi")
+    provider =  IBMProvider(instance="ibm-q-research-2/tu-munich-1/main")
+    backend = provider.get_backend("ibm_nairobi")
 
     #ccircuits = transpile(circuits, backend, optimization_level=3)
     #job = backend.run(ccircuits, shots=20000)
@@ -89,8 +89,8 @@ def main():
     qc_supermarq = qc_supermarq_bench.circuit()
     qc_qiskit = cirq_to_qiskit(qc_supermarq)
 
-    #cqc_qiskit = transpile(qc_qiskit, backend, optimization_level=3)
-    #job = backend.run(cqc_qiskit, shots=20000)
+    cqc_qiskit = transpile(qc_qiskit, backend, optimization_level=3)
+    job = backend.run(cqc_qiskit, shots=20000)
 
     print(qc_qiskit)
 
@@ -105,8 +105,11 @@ def main():
     for q in qernels:
         qc_small = q.get_circuit()
         print(qc_small)
-        #cqc_small = transpile(qc_small, backend, optimization_level=3)
-        #job = backend.run(cqc_small, shots=20000)
+        cqc_small = transpile(qc_small, backend, optimization_level=3)
+        job = backend.run(cqc_small, shots=20000)
+        #results = job.result()
+
+
 
 
 
